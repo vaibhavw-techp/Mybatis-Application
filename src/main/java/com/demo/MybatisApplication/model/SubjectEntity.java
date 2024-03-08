6af4a570c3df0f8ea18d41fae7a7c785d9bb89c5
vaibhav <vaibhav.waghmare@techprescient.com>
2024-03-08 14:22:18 +0530

    Added minor changes

--- src/main/java/com/demo/MybatisApplication/model/SubjectEntity.java
+++ src/main/java/com/demo/MybatisApplication/model/SubjectEntity.java
@@ -1,5 +1,12 @@
 package com.demo.MybatisApplication.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class SubjectEntity {
     private Long subjectId;
     private String name;
