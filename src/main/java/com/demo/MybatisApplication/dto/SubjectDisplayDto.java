b9a492b080e3deac2a5fb5447c29937c8b4cfd34
vaibhav <vaibhav.waghmare@techprescient.com>
2024-03-12 14:32:27 +0530

    Added DTOs for Student, Subject and Teacher

---
+++ src/main/java/com/demo/MybatisApplication/dto/SubjectDisplayDto.java
@@ -0,0 +1,9 @@
+package com.demo.MybatisApplication.dto;
+
+import lombok.Data;
+
+@Data
+public class SubjectDisplayDto {
+    private Long subjectId;
+    private String name;
+}
---
+++ src/main/java/com/demo/MybatisApplication/dto/TeacherDisplayDto.java
@@ -0,0 +1,10 @@
+package com.demo.MybatisApplication.dto;
+
+import lombok.Data;
+
+@Data
+public class TeacherDisplayDto {
+    private String teacherName;
+    private int age;
+    private String email;
+}
