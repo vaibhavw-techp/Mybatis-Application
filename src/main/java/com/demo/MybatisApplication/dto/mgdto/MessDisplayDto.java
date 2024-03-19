1801c3f9798c029bfbb3f268f0cf34a75d9c817d
vaibhav <vaibhav.waghmare@techprescient.com>
2024-03-19 17:38:50 +0530

    Added Dtos for Mess Management

---
+++ src/main/java/com/demo/MybatisApplication/dto/mgdto/MessDisplayDto.java
@@ -0,0 +1,8 @@
+package com.demo.MybatisApplication.dto.mgdto;
+
+public class MessDisplayDto {
+    private Long id;
+    private String name;
+    private int capacity;
+    private String location;
+}
